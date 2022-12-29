0d7cb7c799a820a2fcf59e7c2855d86248c6e3d3
gongxuanzhang <gongxuanzhang@foxmail.com>
2022-12-29 20:42:09 +0800

    词法解析支持 单引号转义，支持左右括号

--- src/main/java/org/gongxuanzhang/mysql/service/token/TokenKind.java
+++ src/main/java/org/gongxuanzhang/mysql/service/token/TokenKind.java
@@ -1,22 +1,27 @@
 package org.gongxuanzhang.mysql.service.token;
 
 /**
- *
  * token种类
+ * 关键字归为一类
+ *
  * @author gxz gongxuanzhang@foxmail.com
  **/
-public enum TokenKind  {
+public enum TokenKind {
 
 
-    KEYWORD,
+    /**
+     * 标准字符串
+     **/
+    LITERACY,
 
-    //
-
+    /**
+     * 一个变量
+     **/
     VAR,
 
+    LEFT_PAREN,
 
-
-
+    RIGHT_PAREN
 
 
 }
--- src/main/java/org/gongxuanzhang/mysql/service/token/Tokenizer.java
+++ src/main/java/org/gongxuanzhang/mysql/service/token/Tokenizer.java
@@ -1,6 +1,8 @@
 package org.gongxuanzhang.mysql.service.token;
 
 
+import org.gongxuanzhang.mysql.exception.SqlParseException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +26,9 @@ public class Tokenizer {
     private final List<Token> tokenList = new ArrayList<>();
 
 
+    /**
+     * 给表达式加最后一个字符保证在查看next偏移的时候不会出现数组溢出
+     **/
     public Tokenizer(String expression) {
         this.expressionString = expression;
         this.charArray = (expression + "\0").toCharArray();
@@ -34,15 +39,48 @@ public class Tokenizer {
     /**
      * 执行词法分析
      **/
-    public List<Token> process() {
+    public List<Token> process() throws SqlParseException {
         while (offset < length) {
             char c = charArray[offset];
             //  字母直接输出
             if (TokenSupport.isAlphabet(c)) {
                 appendLiteracy();
+            } else {
+                switch (c) {
+                    case '\'':
+                        appendString();
+                        break;
+                    case '(':
+                        pushOneToken(TokenKind.LEFT_PAREN);
+                        break;
+                    case ')':
+                        pushOneToken(TokenKind.RIGHT_PAREN);
+                        break;
+                    case ' ':
+                    case '\t':
+                    case '\r':
+                    case '\n':
+                    case '\0':
+                        this.offset++;
+                        break;
+                    default:
+                        throw new SqlParseException(c + "不能解析");
+                }
             }
+
+
         }
         return this.tokenList;
+    }
+
+    private void appendString() {
+        int start = offset;
+        do {
+            offset++;
+        }
+        while (isFinish() || currentChar() == '\'');
+        String data = new String(charArray, start, offset);
+        this.tokenList.add(new Token(TokenKind.LITERACY, data));
     }
 
     private void appendLiteracy() {
@@ -50,15 +88,24 @@ public class Tokenizer {
         do {
             offset++;
         }
-        while (TokenSupport.isIdentifier(charArray[offset]));
+        while (TokenSupport.isIdentifier(currentChar()));
         String data = new String(charArray, start, offset);
-        KeywordSearcher trie = KeywordSearcher.trie;
-        TokenKind search = trie.search(data);
-        this.tokenList.add(new Token(search, data));
+        this.tokenList.add(new Token(TokenKind.VAR, data));
     }
 
 
+    private void pushOneToken(TokenKind tokenKind) {
+        this.tokenList.add(new Token(tokenKind, currentChar() + ""));
+        offset++;
+    }
 
+    private boolean isFinish() {
+        return this.offset >= this.length;
+    }
+
+    private char currentChar() {
+        return charArray[offset];
+    }
 
 
 }
