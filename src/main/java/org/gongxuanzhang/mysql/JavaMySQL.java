26cfa9ff41df8516d4bccb2fdef49db502ad0db6
gongxuanzhang <gongxuanzhangmelt@gmail.com>
2023-01-19 20:01:09 +0800

    test new computer

--- src/main/java/org/gongxuanzhang/mysql/JavaMySQL.java
+++ src/main/java/org/gongxuanzhang/mysql/JavaMySQL.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaMySQL {
     public static void main(String[] args) {
+
         SpringApplication.run(JavaMySQL.class);
     }
 }
