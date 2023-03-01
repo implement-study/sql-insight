add26bfb16a54c445cfb3b00fab55defd536a706
gongxuanzhang <gongxuanzhang@foxmail.com>
2023-03-01 16:19:56 +0800

    add copy right

--- src/test/kotlin/org/gongxuanzhang/mysql/service/executor/ddl/table/ShowTableTest.kt
+++ src/test/kotlin/org/gongxuanzhang/mysql/service/executor/ddl/table/ShowTableTest.kt
@@ -14,37 +14,25 @@
  * limitations under the License.
  */
 
-package org.gongxuanzhang.mysql.service.executor.ddl.database
+package org.gongxuanzhang.mysql.service.executor.ddl.table
 
-import org.gongxuanzhang.mysql.core.result.SingleRowResult
-import org.gongxuanzhang.mysql.destructuringEquals
-import org.gongxuanzhang.mysql.doSql
-import org.gongxuanzhang.mysql.tool.Context
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import java.io.FileFilter
 
 
 /**
- * @author gongxuanzhang
- */
+ * @author gxz gongxuanzhangmelt@gmail.com
+ **/
 @SpringBootTest
-class ShowDatabasesTest {
+class ShowTableTest {
 
 
     @Test
-    fun showDatabases() {
-        val testDatabases = arrayOf("test1", "test2", "test3")
-        testDatabases.forEach {
-            "create database $it".doSql()
-        }
-        val databases = Context.getHome().listFiles(FileFilter { it.isDirectory })
-        val doSql = "show databases".doSql()
-        assert((doSql as SingleRowResult).destructuringEquals(databases?.map { it.name }))
-        testDatabases.forEach {
-            "drop database $it".doSql()
-        }
+    fun showTablesTest() {
+        val database = "show tables"
+
     }
+
 
 
 }
