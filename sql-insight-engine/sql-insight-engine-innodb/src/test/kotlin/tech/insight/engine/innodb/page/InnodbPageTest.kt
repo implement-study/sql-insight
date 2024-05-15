23d3874ae0bd5bd25bb690c2ef5220fe97e287f2
gongxuanzhang <gongxuanzhang@foxmail.com>
2024-05-15 18:12:30 +0800

    add test case

--- sql-insight-engine/sql-insight-engine-innodb/src/test/kotlin/tech/insight/engine/innodb/page/InnodbPageTest.kt
+++ sql-insight-engine/sql-insight-engine-innodb/src/test/kotlin/tech/insight/engine/innodb/page/InnodbPageTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import tech.insight.core.bean.value.Value
 import tech.insight.core.bean.value.ValueInt
 import tech.insight.core.engine.SqlPipeline
 import tech.insight.core.environment.TableManager
@@ -46,7 +47,21 @@ class InnodbPageTest {
             on { indexKey() } doReturn arrayOf(ValueInt(40))
         }
         val key = record.indexKey()
-        innodbPage.findTargetSlot(record)
+        val targetSlot = innodbPage.findTargetSlot(record)
+        val big = innodbPage.getUserRecordByOffset(innodbPage.pageDirectory.slots[targetSlot].toInt())
+        val small = innodbPage.getUserRecordByOffset(innodbPage.pageDirectory.slots[targetSlot + 1].toInt())
+        assert(compareIndexKey(key, big.indexKey()) < 0)
+        assert(compareIndexKey(key, small.indexKey()) > 0)
+    }
+
+    private fun compareIndexKey(aKey: Array<Value<*>>, bKey: Array<Value<*>>): Int {
+        for (i in aKey.indices) {
+            val compare: Int = aKey[i].compareTo(bKey[i])
+            if (compare != 0) {
+                return compare
+            }
+        }
+        return 0
     }
 
 }
